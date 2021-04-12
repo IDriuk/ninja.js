b0326b5bffe2c7f0c5dbd5594627a0896a2632f2
IDriuk <veytarn@gmail.com>
2021-04-12 14:01:38 +0300

    move pagination controls rendering inside ul

--- src/Table/components/Pagination.jsx
+++ src/Table/components/Pagination.jsx
@@ -3,23 +3,20 @@ import React from 'react'
 import PaginationControl from './PaginationControl'
 
 const Pagination = ({ currentPageNumber, totalNumberOfPages, onChange }) => {
-  const pages =
-    Array
-      .from(Array(totalNumberOfPages).keys())
-      .map(pageNumber => {
-        return <PaginationControl
-          key={pageNumber}
-          currentPageNumber={currentPageNumber}
-          pageNumber={pageNumber}
-          onChange={onChange} />
-      })
+  const pages =  Array .from(Array(totalNumberOfPages).keys())
 
   if (pages.length <= 1) {
     return null
   }
+
   return(
     <ul className="pagination">
-      {pages}
+      {pages.map(pageNumber =>  <PaginationControl
+          key={pageNumber}
+          currentPageNumber={currentPageNumber}
+          pageNumber={pageNumber}
+          onChange={onChange} />
+      )}
     </ul>
   )
 }
