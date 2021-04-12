b5cfec8aadfba878b3dfe9015f7e4e05d4e31e30
IDriuk <veytarn@gmail.com>
2021-04-12 14:28:40 +0300

    rename pages to pageNumbers

--- src/Table/components/Pagination.jsx
+++ src/Table/components/Pagination.jsx
@@ -3,15 +3,15 @@ import React from 'react'
 import PaginationControl from './PaginationControl'
 
 const Pagination = ({ currentPageNumber, totalNumberOfPages, onChange }) => {
-  const pages =  [...Array(totalNumberOfPages).keys()]
+  const pageNumbers =  [...Array(totalNumberOfPages).keys()]
 
-  if (pages.length <= 1) {
+  if (pageNumbers.length <= 1) {
     return null
   }
 
   return(
     <ul className="pagination">
-      {pages.map(pageNumber =>  <PaginationControl
+      {pageNumbers.map(pageNumber =>  <PaginationControl
           key={pageNumber}
           currentPageNumber={currentPageNumber}
           pageNumber={pageNumber}
